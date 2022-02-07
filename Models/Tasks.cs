8ee8c9ba617dcb3166fd321033257395fdd1bd1a
swbrock <90523707+swbrock@users.noreply.github.com>
2022-02-06 17:53:55 -0700

    Add project files.

---
+++ Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class Tasks
+    {
+//  Task (Required)
+//  Due Date
+//  Quadrant(Required)
+//  CategoryId
+//  CategoryName
+//  Completed(True/False)
+    }
+}
---
+++ Models/TasksContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class TasksContext : DbContext
+    {
+        //Category (Dropdown containing options: Home, School, Work, Church)
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = ""},
+                new Category { CategoryId = 2, CategoryName = "" },
+                new Category { CategoryId = 3, CategoryName = "" },
+                new Category { CategoryId = 4, CategoryName = "" }
+            );
+        }
+    }
+}
