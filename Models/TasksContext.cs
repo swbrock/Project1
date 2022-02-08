cf874be88f7e14def4cbdb21b1d36a6c00b6fbbe
mina-m-chia <90296893+mina-m-chia@users.noreply.github.com>
2022-02-07 17:24:50 -0700

    Models & Database
    I did migrations and updated the database.

--- Models/TasksContext.cs
+++ Models/TasksContext.cs
@@ -8,16 +8,50 @@ namespace Project1.Models
 {
     public class TasksContext : DbContext
     {
-        //Category (Dropdown containing options: Home, School, Work, Church)
+        //Constructor
+        public TasksContext(DbContextOptions<TasksContext> options) : base (options)
+        {
+            //leave blank
+        }
+
+        public DbSet<TaskEntry> Tasks { get; set; }
+
         public DbSet<Category> Categories { get; set; }
+
+        //Seed data
         protected override void OnModelCreating(ModelBuilder mb)
         {
             mb.Entity<Category>().HasData(
-                new Category { CategoryId = 1, CategoryName = ""},
-                new Category { CategoryId = 2, CategoryName = "" },
-                new Category { CategoryId = 3, CategoryName = "" },
-                new Category { CategoryId = 4, CategoryName = "" }
-            );
+
+                new Category { CategoryId = 1, CategoryName = "Home"},
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+
+                );
+
+            mb.Entity<TaskEntry>().HasData(
+
+                new TaskEntry
+                {
+                    TaskId = 1,
+                    Task = "Get an A in IS 413",
+                    DueDate = "04/23/2022",
+                    CategoryId = 2,
+                    Quadrant = 2,
+                    Completed = false
+                },
+
+                new TaskEntry
+                {
+                    TaskId = 2,
+                    Task = "Get flowers for Professor Hilton",
+                    DueDate = "02/14/2022",
+                    CategoryId = 2,
+                    Quadrant = 2,
+                    Completed = false
+                }
+                );
         }
     }
 }
